f49e14945be747e19b4113f1dcd0426c7dfafde7
Aayush Mall <aayush@Aayushs-MacBook-Air.local>
2024-10-01 21:57:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Topics from './components/Topics';
+
+// require('dotenv');
 
 function App() {
+  const [token, setToken] = useState('');
+  const [completedSubtopics, setCompletedSubtopics] = useState([]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token ? (
+        <Login setToken={setToken} setCompletedSubtopics={setCompletedSubtopics} />
+      ) : (
+        <Topics token={token} completedSubtopics={completedSubtopics} setCompletedSubtopics={setCompletedSubtopics} />
+      )}
     </div>
   );
 }
